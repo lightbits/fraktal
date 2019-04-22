c466ab5773c2885433c9ba31428740f124e78718
lightbits <simenh.jobb@gmail.com>
2019-04-22 20:24:22 +0200

    better error message in parser

--- src/fraktal_parse.h
+++ src/fraktal_parse.h
@@ -186,83 +186,83 @@ static bool parse_string(const char **c, const char **v, size_t *len)
 
 static bool parse_int2(const char **c, int2 *v)
 {
-    if (!parse_char(c, '('))  { parse_error(*c, "Error parsing integer tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_int(c, &v->x)) { parse_error(*c, "Error parsing integer tuple: first component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->y)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ')'))  { parse_error(*c, "Error parsing integer tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))  { parse_error(*c, "integer tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_int(c, &v->x)) { parse_error(*c, "1st tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "integer tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->y)) { parse_error(*c, "2nd tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ')'))  { parse_error(*c, "integer tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_int3(const char **c, int3 *v)
 {
-    if (!parse_char(c, '('))  { parse_error(*c, "Error parsing integer tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_int(c, &v->x)) { parse_error(*c, "Error parsing integer tuple: first component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->y)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->z)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ')'))  { parse_error(*c, "Error parsing integer tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))  { parse_error(*c, "integer tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_int(c, &v->x)) { parse_error(*c, "1st tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->y)) { parse_error(*c, "2nd tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->z)) { parse_error(*c, "3rd tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ')'))  { parse_error(*c, "integer tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_int4(const char **c, int4 *v)
 {
-    if (!parse_char(c, '('))  { parse_error(*c, "Error parsing integer tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_int(c, &v->x)) { parse_error(*c, "Error parsing integer tuple: first component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->y)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->z)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ','))  { parse_error(*c, "Error parsing integer tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_int(c, &v->w)) { parse_error(*c, "Error parsing integer tuple: second component must be an integer.\n"); return false; }
-    if (!parse_char(c, ')'))  { parse_error(*c, "Error parsing integer tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))  { parse_error(*c, "integer tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_int(c, &v->x)) { parse_error(*c, "1st tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->y)) { parse_error(*c, "2nd tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->z)) { parse_error(*c, "3rd tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ','))  { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_int(c, &v->w)) { parse_error(*c, "4th tuple component must be an integer.\n"); return false; }
+    if (!parse_char(c, ')'))  { parse_error(*c, "integer tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_angle2(const char **c, angle2 *v)
 {
-    if (!parse_char(c, '('))        { parse_error(*c, "Error parsing angle tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_angle(c, &v->theta)) { parse_error(*c, "Error parsing angle tuple: first component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))        { parse_error(*c, "Error parsing angle tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_angle(c, &v->phi))   { parse_error(*c, "Error parsing angle tuple: second component must be a number.\n"); return false; }
-    if (!parse_char(c, ')'))        { parse_error(*c, "Error parsing angle tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))        { parse_error(*c, "angle tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_angle(c, &v->theta)) { parse_error(*c, "1st tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))        { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_angle(c, &v->phi))   { parse_error(*c, "2nd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ')'))        { parse_error(*c, "angle tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_float2(const char **c, float2 *v)
 {
-    if (!parse_char(c, '('))    { parse_error(*c, "Error parsing tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_float(c, &v->x)) { parse_error(*c, "Error parsing tuple: first component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))    { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->y)) { parse_error(*c, "Error parsing tuple: second component must be a number.\n"); return false; }
-    if (!parse_char(c, ')'))    { parse_error(*c, "Error parsing tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))    { parse_error(*c, "tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_float(c, &v->x)) { parse_error(*c, "1st tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->y)) { parse_error(*c, "2nd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ')'))    { parse_error(*c, "tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_float3(const char **c, float3 *v)
 {
-    if (!parse_char(c, '('))   { parse_error(*c, "Error parsing tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_float(c, &v->x)) { parse_error(*c, "Error parsing tuple: first component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))   { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->y)) { parse_error(*c, "Error parsing tuple: second component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))   { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->z)) { parse_error(*c, "Error parsing tuple: third component must be a number.\n"); return false; }
-    if (!parse_char(c, ')'))   { parse_error(*c, "Error parsing tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))    { parse_error(*c, "tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_float(c, &v->x)) { parse_error(*c, "1st tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->y)) { parse_error(*c, "2nd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->z)) { parse_error(*c, "3rd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ')'))    { parse_error(*c, "tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
 static bool parse_float4(const char **c, float4 *v)
 {
-    if (!parse_char(c, '('))   { parse_error(*c, "Error parsing tuple: must begin with parenthesis.\n"); return false; }
-    if (!parse_float(c, &v->x)) { parse_error(*c, "Error parsing tuple: first component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))   { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->y)) { parse_error(*c, "Error parsing tuple: second component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))   { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->z)) { parse_error(*c, "Error parsing tuple: third component must be a number.\n"); return false; }
-    if (!parse_char(c, ','))   { parse_error(*c, "Error parsing tuple: components must be seperated by ','.\n"); return false; }
-    if (!parse_float(c, &v->w)) { parse_error(*c, "Error parsing tuple: third component must be a number.\n"); return false; }
-    if (!parse_char(c, ')'))   { parse_error(*c, "Error parsing tuple: must end with parenthesis.\n"); return false; }
+    if (!parse_char(c, '('))    { parse_error(*c, "tuple must begin with parenthesis.\n"); return false; }
+    if (!parse_float(c, &v->x)) { parse_error(*c, "1st tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->y)) { parse_error(*c, "2nd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->z)) { parse_error(*c, "3rd tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ','))    { parse_error(*c, "tuple components must be seperated by ','.\n"); return false; }
+    if (!parse_float(c, &v->w)) { parse_error(*c, "4th tuple component must be a number.\n"); return false; }
+    if (!parse_char(c, ')'))    { parse_error(*c, "tuple must end with parenthesis.\n"); return false; }
     return true;
 }
 
@@ -295,7 +295,7 @@ static bool parse_next_in_list(const char **c)
     if (parse_char(c, ')'))
         return false;
     if (!parse_list_first)
-        if (!parse_char(c, ',')) { parse_error(*c, "Error parsing list: components must be seperated by ','.\n"); return false; }
+        if (!parse_char(c, ',')) { parse_error(*c, "arguments must be seperated by ','.\n"); return false; }
     parse_blank(c);
     parse_list_first = false;
     return true;
@@ -321,8 +321,9 @@ static void parse_list_unexpected()
         if (parse_match(c, name)) \
         { \
             parse_blank(c); \
-            if (!parse_char(c, '=')) { parse_error(*c, "Error parsing argument: expected '=' between identifier and value.\n"); return false; } \
-            if (!parse_##type(c, v)) { parse_error(*c, "Error parsing argument value: unexpected type after '='.\n"); return false; } \
+            if (!parse_char(c, '=')) { parse_error(*c, "expected '=' between argument name and value.\n"); return false; } \
+            parse_blank(c); \
+            if (!parse_##type(c, v)) { parse_error(*c, "unexpected expression after '='.\n"); return false; } \
             return true; \
         } \
         return false; \
@@ -358,13 +359,7 @@ static bool parse_param_meta(const char **c, fParams *p, int param)
     fParamType type = p->type[param];
     while (parse_next_in_list(c)) {
         if (type == FRAKTAL_PARAM_FLOAT ||
-            type == FRAKTAL_PARAM_FLOAT_VEC2 ||
-            type == FRAKTAL_PARAM_FLOAT_VEC3 ||
-            type == FRAKTAL_PARAM_FLOAT_VEC4 ||
-            type == FRAKTAL_PARAM_INT ||
-            type == FRAKTAL_PARAM_INT_VEC2 ||
-            type == FRAKTAL_PARAM_INT_VEC3 ||
-            type == FRAKTAL_PARAM_INT_VEC4)
+            type == FRAKTAL_PARAM_INT)
         {
             if (parse_argument_float(c, "mean", (float*)&p->mean[param])) continue;
             else if (parse_argument_float(c, "scale", (float*)&p->scale[param])) continue;
@@ -396,7 +391,7 @@ static bool parse_param_meta(const char **c, fParams *p, int param)
 
     if (!parse_end_list(c))
     {
-        parse_error(*c, "Failed to parse parameter meta list.\n");
+        parse_error(*c, "invalid parameter meta arguments.\n");
         return false;
     }
     return true;
